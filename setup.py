37a4e0728ac85a060bb3b909df6bcd0223700982
Tomasz Łakota <tomasz.lakota@gmail.com>
2018-07-27 19:15:48 +0200

    Initial version tested with test.pypi.org

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="stateflow",
+    version="0.0.1",
+    author="Tomasz Łakota",
+    author_email="tomasz@lakota.pl",
+    description="A state-propagation framework.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/peper0/stateflow",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
